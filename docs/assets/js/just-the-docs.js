910098464756a2ead393f2a939b3206fce9b96ac
Shomil Jain <shomil@berkeley.edu>
2020-12-22 23:00:14 -0800

    fixed

--- docs/assets/js/just-the-docs.js
+++ docs/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://mdbdev.github.io/react-native-training-program-site//assets/js/search-data.json', true);
+  request.open('GET', 'https://mdbdev.github.io/react-native-training-program-site/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://mdbdev.github.io/react-native-training-program-site//assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://mdbdev.github.io/react-native-training-program-site/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
